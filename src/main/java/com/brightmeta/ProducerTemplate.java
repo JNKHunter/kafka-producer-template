1527824d1329049e6e01dbe47b1e8653bcf4c2b9
John Hunter <john@pushagency.io>
2017-09-06 15:06:59 -0400

    changed class to better name

--- src/main/java/com/brightmeta/ProducerTemplate.java
+++ src/main/java/com/brightmeta/ProducerTemplate.java
@@ -1,5 +1,8 @@
 package com.brightmeta;
 
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
@@ -7,7 +10,7 @@ import java.util.Properties;
 /**
  * Created by John on 9/6/17.
  */
-public class KafkaProducer {
+public class ProducerTemplate {
 
     public static void main(String[] args) {
         Properties properties = new Properties();
@@ -17,5 +20,15 @@ public class KafkaProducer {
         properties.setProperty("value.serializer", StringSerializer.class.getName());
         properties.setProperty("acks", "1");
         properties.setProperty("retries", "3");
+        properties.setProperty("linger.ms", "1");
+
+        Producer<String, String> producer =
+                new org.apache.kafka.clients.producer.KafkaProducer<String, String>(properties);
+
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<String, String>("test", "3", "message test");
+
+        producer.send(producerRecord);
+        producer.close();
     }
 }
